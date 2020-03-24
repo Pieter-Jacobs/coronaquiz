970b5d8138b4d283c8a14c238f4f21bc305683bc
Pieter-Jacobs <pieternee@gmail.com>
2020-03-24 20:23:02 +0100

    Styled the home page

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -12,15 +12,17 @@ export default function SignIn(props) {
     setStart(true);
   }
   return (
-    <div>
-      <div className={start ? styles['invisible'] : styles['qc-home']}>
-        <h1>Welkom bij de Wekelijkse Corona Quiz!</h1>
-        <h2>Doe mee om te laten zien dat jij de slimste bent.</h2>
-        <h3>Of gewoon omdat je geen flikker te doen hebt.</h3>
+    <div className={styles['cq-main-container']}>
+      <div className={start ? styles['invisible'] : styles['cq-home']}>
+        <div className={styles['cq-title-container']}>
+          <h1>Welkom bij de Wekelijkse Corona Quiz!</h1>
+          {/*<h2>DGwoon omdat je toch niks te doen hebt </h2>*/}
+        </div>
         <form className={styles['cq-form']} onSubmit={handleSubmit}>
           <input className={styles['cq-username']} type="text" placeholder="Gebruikersnaam..." onChange={handleInputChange}/>
           <input className={styles['cq-start-button']} type="submit" value="corGOna" onClick={startGame}/>
         </form>
+
       </div>
     {start == true && <Quiz username={inputs.username}/>}
     </div>
